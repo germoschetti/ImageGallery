230c606d03f2228b45658baa371210d73b77ef5b
German Moschetti <germoschetti@hotmail.com>
2021-08-30 14:22:13 -0300

    creating angular project with its components and services

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+// Components
+
+import { AppComponent } from './app.component';
+import { SearchImageComponent } from './components/search-image/search-image.component';
+import { ShowImageComponent } from './components/show-image/show-image.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ErrorComponent } from './shared/error/error.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchImageComponent,
+    ShowImageComponent,
+    NavbarComponent,
+    ErrorComponent,
+    SpinnerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
